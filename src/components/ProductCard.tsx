2909ba77b0db793ac5496e1178114c1fe38cea29
ferin <sudawan91@gmail.com>
2024-11-08 14:07:03 +0700

    update image

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 // src/components/ProductCard.tsx
 import React from 'react';
+import rainbow from '../assets/rainbow.jpg'
 
 
 type ProductCardProps = {
@@ -12,7 +13,7 @@ type ProductCardProps = {
 const ProductCard: React.FC<ProductCardProps> = ({ name, weight, price, imageUrl }) => {
   return (
     <div className="border p-4 rounded-lg ">
-      <img src={imageUrl} alt={name} className="w-full h-auto mb-2" />
+      <img src="https://monsoontea.co.th/cdn/shop/files/RainbowBlend50gtinWeb_1024x1024@2x.jpg?v=1695122791" alt={name} className="w-full h-auto mb-2" />
       <h2 className="text-md font-semibold">{name}</h2>
       <p className="text-sm">{weight}</p>
       <p className="text-md font-bold">{price}</p>
