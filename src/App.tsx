2746d81bee0c8ee61815a5640cf86b4324d50ff0
BLUE biyayeetahe <burahan206@gmail.com>
2024-11-08 11:49:30 +0700

    Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/Home";
+
 
 
 
@@ -10,8 +8,6 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Home/>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div
+        className="flex flex-col justify-center pl-20 bg-fixed bg-cover bg-center   min-h-screen min-w-screen     "
+        style={{ backgroundImage: `url('/src/assets/img/Rectangle 2.svg')` }}
+      >
+        <div className="me-8 flex flex-col text-left sm:justify-center">
+          <h1 className="text-xl w-37 font-semibold  text-white ">
+            อยากรู้ไหมว่าคุณเหมาะกับชาแบบไหน ?
+          </h1>
+          <br />
+          <br />
+          <h1 className="text-xl min-w-56 font-semibold  text-white ">
+            ให้ MILKY TEA-REX แนะนำให้ คุณใช้เวลาเพียง 1 นาทีเท่านั้น
+          </h1>
+          <br />
+          <button className="px-4 py-2 rounded-full bg-red-500 text-white w-28 h-10">
+            เริ่มเลย
+          </button>
+          <br />
+          <br />
+          <div className="flex items-center text-white underline">
+            <img
+              src="/src/assets/img/ของขวัญ.svg"
+              alt="gift icon"
+              className="mr-2 w-6 h-6"
+            />
+            <a href='#'>แด่คนพิเศษในแบบคุณ</a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
