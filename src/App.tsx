ac7e35724f2bf5eceda4df996b5e8ceffa5d1a46
nutza <www.nutzazaza@gmail.com>
2024-11-11 09:35:02 +0700

    navbar-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Review from './components/Review'
+import Navbar from './components/Navbar'
+import Test from './test'
+
 
 
 
@@ -10,8 +12,12 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
+    <>
+    <Navbar />
+    {/* <Review /> */}
+    {/* <Test /> */}
+   
+     </>
   )
 }
