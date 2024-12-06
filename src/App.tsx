fcdaef9e4aacdec0253f3bcc0fc853f7a7a0fd00
Thitithorn <Thitithorn.su@gmail.com>
2024-12-06 13:30:30 +0700

    updateApp

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 // import Loading from './pages/Loading';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import BlogPage from './pages/BlogPage';
-import Article from './components/Article'; 
-
-
-
-
+import Article from './components/Article';
 // import Tea from './pages/Tea';
 // import PaymentPopup from './pages/PaymentPopup';
 // import Shopchoice from './components/Shopchoice';
-
 // import ProductDetailPage from './pages/ProductDetailPage';
 
 const App: React.FC = () => {
+  const handleLogin = (credentials: { email: string; password: string }) => {
+    console.log("Logging in with:", credentials);
+    // ทำการล็อกอินที่นี่
+  };
 
-  return ( 
-  <Router>
-    <Navbar />
-    <div className="content" style={{ paddingTop: '100px', paddingBottom:'30px'}}>
+  const handleSignUp = (userData: { username: string; email: string; password: string }) => {
+    console.log("Signing up with:", userData);
+    // ทำการสมัครสมาชิกที่นี่
+  };
+
+  return (
+    <Router>
+      <Navbar />
+      <div className="content" style={{ paddingTop: '100px', paddingBottom: '30px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/signup" element={<Signup onSignUp={handleSignUp} />} />
           <Route path="/products" element={<ProductListPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/green-tea" element={<ProductListPage />} />
@@ -40,11 +46,11 @@ const App: React.FC = () => {
           <Route path="/article/:id" element={<Article />} />
         </Routes>
       </div>
-      <Footer/>
-  </Router>
-);  
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
-    
-    
+
+
