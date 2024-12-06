80849f46424baa52a5e2105dff2032f34ec74469
Thitithorn <Thitithorn.su@gmail.com>
2024-12-06 10:54:13 +0700

    UpdateBytong

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Loading from './pages/Loading';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Loading from './pages/Loading';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import BlogPage from './pages/BlogPage';
@@ -16,9 +16,9 @@ import Article from './components/Article';
 
 
 
-import Tea from './pages/Tea';
-import PaymentPopup from './pages/PaymentPopup';
-import Shopchoice from './components/Shopchoice';
+// import Tea from './pages/Tea';
+// import PaymentPopup from './pages/PaymentPopup';
+// import Shopchoice from './components/Shopchoice';
 
 // import ProductDetailPage from './pages/ProductDetailPage';
 
@@ -42,32 +42,7 @@ const App: React.FC = () => {
       </div>
       <Footer/>
   </Router>
-);
-  return (
-    <Router>
-      <Navbar />
-      
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="/products" element={<ProductListPage />} />
-        {/* <Route path="/products/:id" element={<ProductDetailPage />} />
-        <Route path="/green-tea" element={<ProductListPage />} />
-        <Route path="/black-tea" element={<ProductListPage />} />
-        <Route path="/white-tea" element={<ProductListPage />} />
-        <Route path="/oolong-tea" element={<ProductListPage />} />
-        
-        <Route path="/oolong-tea" element={<ProductListPage />} /> */}
-      </Routes>
-      
-      
-      
-      {/* <Footer /> */}
-    </Router>
-  );
+);  
 };
 
 export default App;
